9af14710dc6034717df1fd7b7a8f01376cfcf051
saleh <saleh_jamali@ymail.com>
2020-01-14 06:45:06 -0500

    some comments

--- inc/ocl_imp/xilinx/VectorizationHelper.h
+++ inc/ocl_imp/xilinx/VectorizationHelper.h
@@ -16,14 +16,21 @@ struct VectorizedArray{
 #define FlatIdx_to_VecIdx(vecDepth, flatIdx) (((unsigned long)flatIdx)/((unsigned long)vecDepth))
 #define FlatIdx_to_VecSubIdx(vecDepth, flatIdx) (((unsigned long)flatIdx)%((unsigned long)vecDepth))
 
-// Input and output tensor vectorization depth for each of the kernels:(words of 4-bytes)
-// All instances of OclTensors without specified AXI_WIDTH, will be padded to be dividable to CONFIG_M_AXI_WIDTH.
+// * Input and output tensor vectorization depth for each of the kernels:(words of 4-bytes)
+// * All instances of OclTensors without specified AXI_WIDTH, will be padded to be dividable to CONFIG_M_AXI_WIDTH.
+// * Should be a power of two.
 #define CONFIG_M_AXI_WIDTH	16
 
-// AXI bus width for inputTn(only) of task_gather(tensors are still padded to be devidable to CONFIG_M_AXI_WIDTH).
+// * AXI bus width for inputTn(only) of task_gather(tensors are still padded to be devidable to CONFIG_M_AXI_WIDTH).
+// * This will not cause any problem if the respective tensor is padded for 16-words wide m_axi bus, because 
+//   len % 16 is always greater than len % 4, so there won't be any out of bound memory accesses.
+// * Should be a power of two.
 #define CONFIG_GATHER_INPUTTN_M_AXI_WIDTH 4
 
-// AXI bus width for indicesSplitedTn(only) of task_topk(kValue should be devidable to CONFIG_TOPK_OUTPUTTN_M_AXI_WIDTH).
+// * AXI bus width for indicesSplitedTn(only) of task_topk(kValue should be devidable to CONFIG_TOPK_OUTPUTTN_M_AXI_WIDTH).
+// * This will not cause any problem if the respective tensor is padded for 16-words wide m_axi bus, because 
+//   len % 16 is always greater than len % 4, so there won't be any out of bound memory accesses.
+// * Should be a power of two.
 #define CONFIG_TOPK_OUTPUTTN_M_AXI_WIDTH 4
 
 #endif //SDACCEL_CMAKE_SIMPLE_VECTORIZATIONHELPER_H
