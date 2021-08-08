b7855b3eeb4f2e3c9878574488a0e33f2115fe45
saleh <saleh_jamali@ymail.com>
2021-08-07 20:58:27 -0400

    fixed write_checkpoints path(set to abs path instead of local path which was not working)

--- scripts/SlrUtilizationReport.tcl
+++ scripts/SlrUtilizationReport.tcl
@@ -1,4 +1,4 @@
 # Run on tcl.pre route(only for a HW build)
 report_utilization -slr -file ${SlrUtilOutputFile}
 report_utilization -pblocks pblock_pcie -pblocks pblock_SH -pblocks pblock_SH_SHIM -file ${PblockUtilOutputFile}
-write_checkpoint -force design_pre_route.dcp
+write_checkpoint -force ${PreRouteCheckpointFile}
